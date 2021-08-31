28a7581e2da328156db0986a8edab6c0c75400a2
EventBridge <vincentwhitetran@gmail.com>
2021-09-01 09:37:03 +1000

    dynamic board generation

--- game.js
+++ game.js
@@ -10,15 +10,40 @@ for (let i = 0; i < 16*16; i++) {
 }
 
 function clearBoard() {
-    const tiles = document.querySelectorAll('.boxhover');
+    const tiles = document.querySelectorAll('.box');
 
     tiles.forEach((tile) => {
-        tile.classList.remove('boxhover');
+        tile.remove();
     });
+}
+
+const newPrompt = document.querySelector('.newboard');
+const newButton = document.querySelector('.newboard button');
+
+newButton.addEventListener('click', () => {
+    const size = document.querySelector('.newboard input').value;
+    if (size > 100) {
+        return;
+    }
+    newPrompt.style.visibility = 'hidden';
+    newBoard(size);
+});
+
+function newBoard(size) {
+    container.style.cssText = `grid-template-columns: repeat(${size}, auto)`
+    for (let i = 0; i < size*size; i++) {
+        const box = document.createElement('div');
+        box.addEventListener('mouseover', () => {
+            box.classList.add('boxhover')
+        });
+        box.classList.add('box');
+        container.appendChild(box);
+    }
 }
 
 const clearButton = document.querySelector('.clear');
 
 clearButton.addEventListener('click', () => {
     clearBoard();
+    newPrompt.style.visibility = 'visible';
 });
