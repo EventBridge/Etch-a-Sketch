abbdb7f6ded90170113267bdff175d394e297c3b
EventBridge <vincentwhitetran@gmail.com>
2021-09-01 08:55:09 +1000

    draw and clear the board

--- game.js
+++ game.js
@@ -2,6 +2,23 @@ const container = document.querySelector('.container');
 
 for (let i = 0; i < 16*16; i++) {
     const box = document.createElement('div');
+    box.addEventListener('mouseover', () => {
+        box.classList.add('boxhover')
+    });
     box.classList.add('box');
     container.appendChild(box);
 }
+
+function clearBoard() {
+    const tiles = document.querySelectorAll('.boxhover');
+
+    tiles.forEach((tile) => {
+        tile.classList.remove('boxhover');
+    });
+}
+
+const clearButton = document.querySelector('.clear');
+
+clearButton.addEventListener('click', () => {
+    clearBoard();
+});
